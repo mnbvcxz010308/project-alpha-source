89bf5b55d4a41d7f1247b9aada08665214b3c5aa
= <vipings@aol.in>
2017-11-06 11:47:17 +0530

    Full Blog

---
+++ content/articles/2100/ipynb-liquid.md
@@ -0,0 +1,9 @@
+Title: Jupyter notebook test for liquid tags
+Slug: jupyter-notebook-liquid-test
+Date: 2100-12-31
+Tags: Test
+Author: Vipin G S
+Include: requirejs,jquery,bokeh
+Status: draft
+
+{% notebook articles/2100/ipynb.ipynb %}
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,38 +1,64 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Vipin G S'
-SITENAME = 'EssentialDataScience'
-SITEURL = 'https://blog.vipings.com'
+LOAD_CONTENT_CACHE = False
 
-PATH = 'content'
+SITEURL = 'https://blog.vipings.com'
+# SITEURL = 'http://danielfrg.github.io'
+AUTHOR = u'Vipin G S'
+SITENAME = u'EssentialDataScience'
 
 TIMEZONE = 'Asia/Kolkata'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
 MARKUP = ('md', 'ipynb')
 
-PLUGIN_PATHS = './plugins'
-PLUGINS = ['ipynb.markup']
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+DEFAULT_DATE_FORMAT = '%B %d, %Y'
+
+SUMMARY_MAX_LENGTH = 150
+DEFAULT_PAGINATION = 10
+
+PAGE_DIRS = ['pages']
+ARTICLE_DIRS = ['articles']
+
+THEME = 'theme'
+STATIC_PATHS = ['images']
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+PAGE_SAVE_AS = '{category}/{slug}.html'
+PAGE_URL = PAGE_SAVE_AS
+
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'codehilite'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
+
+# Paths are relative to `content`
+STATIC_PATHS = ['images', 'favicon.ico', '404.html', 'robots.txt', 'CNAME']
+
+# THEME SETTINGS
+DEFAULT_HEADER_BG = '/images/station1.jpg'
+ABOUT_PAGE = '/pages/about.html'
+TWITTER_USERNAME = 'vipingsnair'
+GITHUB_USERNAME = 'vipings'
+SHOW_ARCHIVES = True
+SHOW_FEED = True
+
+GOOGLE_ANALYTICS_CODE = 'UA-35523657-2'
+GOOGLE_ANALYTICS_DOMAIN = 'danielfrg.com'
+
+# PLUGINS SETTINGS
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['sitemap', 'ipynb.markup', 'ipynb.liquid', 'liquid_tags.youtube', 'liquid_tags.b64img']
+
+SITEMAP = {
+    'format': 'xml'
+}
+
+IPYNB_EXTEND_STOP_SUMMARY_TAGS = [('h2', None), ('ol', None), ('ul', None)]
